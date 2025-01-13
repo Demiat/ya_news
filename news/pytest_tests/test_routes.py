ca0257b9bdd5ebe5127419400c52571367ec9812
Дмитрий Тарасов <demiat@mail.ru>
2025-01-14 00:28:15 +0300

    pytest rout ready

--- news/pytest_tests/test_routes.py
+++ news/pytest_tests/test_routes.py
@@ -2,6 +2,9 @@ from http import HTTPStatus
 
 from django.urls import reverse
 from pytest import mark, lazy_fixture
+from pytest_django.asserts import assertRedirects
+
+COMMENT_EDIT_DEL_PATHS = ('reverse_name', ('news:edit', 'news:delete'))
 
 
 @mark.django_db
@@ -28,12 +31,18 @@ def test_pages_availability_for_anonymous_user(client, revers_name, args):
         (lazy_fixture('reader_client'), HTTPStatus.NOT_FOUND),
     )
 )
-@mark.parametrize(
-    'reverse_name',
-    ('news:edit', 'news:delete'),
-)
+@mark.parametrize(*COMMENT_EDIT_DEL_PATHS)
 def test_page_edit_and_delete_comment(client, status, reverse_name, comment):
     """Доступ к страницам редактирования и удаления комментария"""
     url = reverse(reverse_name, args=(comment.id,))
     response = client.get(url)
     assert response.status_code == status
+
+
+@mark.django_db
+@mark.parametrize(*COMMENT_EDIT_DEL_PATHS)
+def test_anonymouse_cannot_edit_delete_comment(reverse_name, client, comment):
+    """Аноним при запросе на изменения ресурсов перенаправляется на логин"""
+    url = reverse(reverse_name, args=(comment.id,))
+    response = client.get(url)
+    assertRedirects(response, f'{reverse("users:login")}?next={url}')
